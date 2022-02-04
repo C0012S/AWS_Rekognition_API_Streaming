04cadcc78d1a99be4a81fd3c19312996dfaec03e
C0012S <qkrtkd1002@duksung.ac.kr>
2022-02-04 17:03:40 +0900

    Create Collection

---
+++ CreateCollection.py
@@ -0,0 +1,34 @@
+# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# PDX-License-Identifier: MIT-0 (For details, see https://github.com/awsdocs/amazon-rekognition-developer-guide/blob/master/LICENSE-SAMPLECODE.)
+
+import boto3
+import csv
+
+with open('Srek_credentials.csv', 'r') as input:
+    next(input)
+    reader = csv.reader(input)
+    for line in reader:
+        access_key_id = line[2]
+        secret_access_key = line[3]
+
+def create_collection(collection_id):
+    client = boto3.client('rekognition',
+                          aws_access_key_id=access_key_id,
+                          aws_secret_access_key=secret_access_key,
+                          region_name="us-east-1")
+
+    # Create a collection
+    print('Creating collection:' + collection_id)
+    response = client.create_collection(CollectionId=collection_id)
+    print('Collection ARN: ' + response['CollectionArn'])
+    print('Status code: ' + str(response['StatusCode']))
+    print('Done...')
+
+
+def main():
+    collection_id = 'StreamingCollection'
+    create_collection(collection_id)
+
+
+if __name__ == "__main__":
+    main()
